ce2e627fafe342df5d606174abe6a12215c40f0e
g-onelli <63609012+g-onelli@users.noreply.github.com>
2021-09-03 16:36:19 -0400

    Add files via upload

---
+++ Hw0-ToolSetup-Onelli.py
@@ -0,0 +1,10 @@
+#Gabriela Onelli
+#Saremi SSW 567
+#09/04/21
+
+#Homework 00 - Tools Setup
+#I pledge my honor that I abided by the Stevens Honor System  -Gabriela Onelli
+
+def greetingPrint():
+    print('Hello world');
+
---
+++ Test-Hw0-ToolSetup-Onelli.py
@@ -0,0 +1,15 @@
+#Gabriela Onelli
+#Saremi SSW 567
+#09/04/21
+
+#Homework 00 - Tools Setup
+#I pledge my honor that I abided by the Stevens Honor System  -Gabriela Onelli
+
+import unittest;
+
+class TestPrint(unittest.TestCase):
+    def testGreeting(self):
+        self.assertEqual("Hello World","Hello World","These are equal");
+
+if __name__ =='__main__':
+    unittest.main();
